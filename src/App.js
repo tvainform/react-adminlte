44c4641a774a014f0814b05e808d17977a84fcf4
tvainform <tvainform@yandex.ru>
2020-09-11 14:25:41 +0500

    release

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom'
 import './App.css';
 import Header from './Components/Header';
@@ -13,28 +13,34 @@ import ContentHeader from "./Components/ContentHeader/ContentHeader";
 import IncidentsContainer from "./Components/Incidents/IncidentsContainer";
 import PersonsContainer from "./Components/Persons/PersonsContainer";
 import UsersContainer from "./Components/Users/UsersContainer";
+import * as firebase from "firebase";
 
-const App = () => {
-  return (
-      <div className="wrapper">
-        <Header/>
-        <Aside/>
-        <div className="content-wrapper">
-          <ContentHeader/>
-          <Switch>
-            <Route exact path="/" render={() => <Main />}/>
-            <Route exact path="/about" render={() => <About />}/>
-            <Route exact path="/persons" render={() => <PersonsContainer/>}/>
-            <Route exact path="/coveralls" render={() => <Coveralls/>}/>
-            <Route exact path="/calendar" render={() => <Calendar/>}/>
-            <Route exact path="/incidents" render={() => <IncidentsContainer/>}/>
-              <Route exact path="/users" render={() => <UsersContainer/>}/>
-            <Route render={() => <Error404/>}/>
-          </Switch>
-        </div>
-        <Footer/>
-      </div>
-  );
+
+export default class App extends Component {
+    componentDidMount(){
+        const db = firebase.database();
+        console.log(db);
+    }
+    render() {
+        return (
+            <div className="wrapper">
+                <Header/>
+                <Aside/>
+                <div className="content-wrapper">
+                    <ContentHeader/>
+                    <Switch>
+                        <Route exact path="/" render={() => <Main/>}/>
+                        <Route exact path="/about" render={() => <About/>}/>
+                        <Route exact path="/persons" render={() => <PersonsContainer/>}/>
+                        <Route exact path="/coveralls" render={() => <Coveralls/>}/>
+                        <Route exact path="/calendar" render={() => <Calendar/>}/>
+                        <Route exact path="/incidents" render={() => <IncidentsContainer/>}/>
+                        <Route exact path="/users" render={() => <UsersContainer/>}/>
+                        <Route render={() => <Error404/>}/>
+                    </Switch>
+                </div>
+                <Footer/>
+            </div>
+        );
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -6,9 +6,22 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import * as firebase from 'firebase'
+import {initializeApp} from "firebase";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyDOncgrRVKkdECjGJd8xJ3xcz7EdnabeHk",
+    authDomain: "incident-itk.firebaseapp.com",
+    databaseURL: "https://incident-itk.firebaseio.com",
+    projectId: "incident-itk",
+    storageBucket: "incident-itk.appspot.com",
+    messagingSenderId: "652187485400",
+    appId: "1:652187485400:web:6b055eed1f22c74e1083ad"
+};
 
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
+
     <BrowserRouter>
         <Provider store={store}>
             <App/>
